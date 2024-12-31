2b15a5b16329ddfce326f442612a85473f46b7d4
jingao.sun <jingao.sun@faw-vw.com>
2024-12-31 17:03:22 +0800

    init

--- doc/router/index.ts
+++ doc/router/index.ts
@@ -69,7 +69,7 @@ export const REDIRECT = {
 };
 
 const router = createRouter({
-  history: createWebHistory('/mo-front-end-util/dist'),
+  history: createWebHistory('/mo-front-end-util/dist/index.html'),
   routes: [
     ...Object.values(ROUTERS).reduce((pre, cur) => {
       pre.push(...cur.children);
